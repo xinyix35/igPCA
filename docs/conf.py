7008796a80b0363c79bae477285fa4c094832901
xinyix35 <xinyix35@uw.edu>
2023-12-05 02:28:45 -0800

    Lines starting

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
 # The master toctree document.
 import sphinx_rtd_theme
 master_doc = 'index'
@@ -36,7 +35,8 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ['recommonmark',
+              'sphinx_rtd_theme',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
