ce464756c5377287391339f28707be2555291485
xinyix35 <xinyix35@uw.edu>
2023-12-05 02:09:27 -0800

    [modify]

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,17 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_automodapi.automodapi',
+    'numpydoc',
+    'nbsphinx',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx_multiversion',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
