fef869072f22869b239f83be0e72b0e4dbfe3422
xinyix35 <xinyix35@uw.edu>
2023-12-05 00:01:47 -0800

    [add] debug :)

---
+++ igPCA/__init__.py
@@ -0,0 +1,13 @@
+
+from . import SelectionMethod, PosteriorMean, GMDRegression, BiCrossValidation, _spilt
+from . import igPCA
+from . import GMD
+name = "igPCA"
+
+all = [
+    "GMD",
+    "igPCA",
+    "Selection",
+]
+
+__version__ = "1.0.0"
